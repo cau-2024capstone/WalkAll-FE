b735950bf819cbe8432fd6e2392205cdf640839a
tesabel <woochan0408@naver.com>
2024-10-30 22:26:18 +0900

    separate TMapView html

--- components/TMapView.js
+++ components/TMapView.js
@@ -1,51 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { WebView } from "react-native-webview";
 
 const TMapView = ({ latitude, longitude }) => {
-  const htmlContent = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="utf-8">
-      <title>TMap Example</title>
-      <script src="https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=(키값)"></script>
-      <style>
-        html, body, #map_div {
-          width: 100%;
-          height: 100%;
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-    </head>
-    <body>
-      <div id="map_div"></div>
-      <script>
-        var map;
-        function initTmap(){
-          map = new Tmapv2.Map("map_div", {
-            center: new Tmapv2.LatLng(${latitude}, ${longitude}),
-            width: "100%",
-            height: "100%",
-            zoom: 15
-          });
-          
-          var marker = new Tmapv2.Marker({
-            position: new Tmapv2.LatLng(${latitude}, ${longitude}),
-            map: map
-            
-          });
-        }
-        initTmap();
-      </script>
-    </body>
-    </html>
-  `;
+  const webViewRef = useRef(null); //웹뷰 참조
+
+  useEffect(() => {
+    if (webViewRef.current) {
+      // HTML 파일 로드 후에 위치 값 전달
+      const message = `initTmap(${latitude}, ${longitude});`;
+      webViewRef.current.injectJavaScript(message);
+    }
+  }, [latitude, longitude]);
 
   return (
     <WebView
+      ref={webViewRef}
       originWhitelist={["*"]}
-      source={{ html: htmlContent }}
+      source={require("./html/TMapView.html")}
       style={{ flex: 1 }}
     />
   );
