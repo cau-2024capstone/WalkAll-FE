62c1191affacfb1797e1d26241c902810ab9c065
tesabel <woochan0408@naver.com>
2024-11-02 11:12:50 +0900

    [fix]call "make route API" and functioning properly

--- components/TMapView.js
+++ components/TMapView.js
@@ -46,25 +46,47 @@ const TMapView = ({ latitude, longitude }) => {
         }
         break;
       case "routeMakeData":
-        routeMakeData = data.routeMakeData;
-        console.log("routeMakeData:", routeMakeData);
+        const routeMakeDataTest = {
+          startX: 127.08010464906738,
+          startY: 37.490008320816926,
+          endX: 127.08683252334636,
+          endY: 37.49037011805361,
+          passList:
+            "127.08157718181657,37.48905699854451_127.0825883746152,37.48900166399026_127.08269119262735,37.491289500793926",
+          reqCoordType: "WGS84GEO",
+          resCoordType: "WGS84GEO",
+          searchOption: 4,
+          startName: "출발지",
+          endName: "도착지",
+        };
+        const routeMakeData = JSON.parse(data.routeMakeData);
+        console.log("경로 생성 데이터:", routeMakeData);
+
+        const urlEncodedData = new URLSearchParams(routeMakeData).toString();
+
         fetch(
           "https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&format=json",
           {
             method: "POST",
             headers: {
               accept: "application/json",
-              appKey: "P4s73F9IVz7sHlNhyg3RC9PwWamafSQb1Zy0g4cy", // appKey 확인
-              "Content-Type": "application/json",
+              appKey: "P4s73F9IVz7sHlNhyg3RC9PwWamafSQb1Zy0g4cy",
+              "Content-Type": "application/x-www-form-urlencoded",
             },
-            body: JSON.stringify(routeMakeData),
+            body: urlEncodedData,
           }
         )
           .then((response) => response.json())
           .then((response) => {
-            console.log("응답 내용:", response);
-            const displayRouteMessage = `displayRoute(response.features, index);`;
-            webViewRef.current.injectJavaScript(displayRouteMessage);
+            if (response && response.features) {
+              const index = 0; // 필요한 경우 적절한 값으로 변경
+              const displayRouteMessage = `displayRoute(${JSON.stringify(
+                response.features
+              )}, ${index});`;
+              webViewRef.current.injectJavaScript(displayRouteMessage);
+            } else {
+              console.log("정상적인 경로 데이터가 아닙니다:", response);
+            }
           })
           .catch((error) => {
             console.log("Error:", error);
