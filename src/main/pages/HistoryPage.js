b4c3396f6e5776320c2caf96a83c6b54fd010299
ming9oo <mj10050203@gmail.com>
2024-11-07 18:43:53 +0900

    [Bug] Bug#6_unable to load stylesheet

--- src/main/pages/HistoryPage.js
+++ src/main/pages/HistoryPage.js
@@ -8,9 +8,7 @@ function HistoryPage() {
         <ScrollView contentContainerStyle={styles.container}>
             <Text style={styles.text}>This is the History Page</Text>
             <HistoryCard title="Route 1" date="2023-01-01" distance="5.2" />
-            <HistoryCard title="Route 2" date="2023-01-02" distance="3.8" />
-            <HistoryCard title="Route 3" date="2023-01-03" distance="7.4" />
-            {/* 필요한 만큼 HistoryCard 추가 */}
+            { }
         </ScrollView>
     );
 }
