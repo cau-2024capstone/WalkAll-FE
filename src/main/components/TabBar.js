86a3474ac1fa7418905cb4d528a6bfd24bfe5c8c
ming9oo <mj10050203@gmail.com>
2024-11-12 21:23:00 +0900

    [fix] change file directories and TabBar.js

--- src/main/components/TabBar.js
+++ src/main/components/TabBar.js
@@ -4,7 +4,13 @@ import { useNavigation } from '@react-navigation/native';
 import { Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import HistoryScreen from '../screens/HistoryScreen';
-import RouteScreen from '../screens/RouteScreen';
+import { createStackNavigator } from "@react-navigation/stack";
+import StartPointSelection from "../components/routeScreen/StartPointSelection";
+import WaypointSetting from "../components/routeScreen/WaypointSetting";
+
+import DestinationSetting from "../components/routeScreen/DestinationSetting";
+import RecommendedRoutes from "../components/routeScreen/RecommendedRoutes";
+import NavigationScreen from "../components/routeScreen/NavigationScreen";
 import rootStyles from "../styles/StyleGuide";
 
 const Tab = createBottomTabNavigator(); // 하단 탭 선언
@@ -13,6 +19,30 @@ const Tab = createBottomTabNavigator(); // 하단 탭 선언
 
 function MyPage() {
     return null;
+}
+
+const RouteStack = createStackNavigator();
+
+// 경로 생성 스택 네비게이터
+function RouteStackScreen() {
+    return (
+        <RouteStack.Navigator screenOptions={{ headerShown: false }}>
+            <RouteStack.Screen
+                name="StartPointSelection"
+                component={StartPointSelection}
+            />
+            <RouteStack.Screen name="WaypointSetting" component={WaypointSetting} />
+            <RouteStack.Screen
+                name="DestinationSetting"
+                component={DestinationSetting}
+            />
+            <RouteStack.Screen
+                name="RecommendedRoutes"
+                component={RecommendedRoutes}
+            />
+            <RouteStack.Screen name="NavigationScreen" component={NavigationScreen} />
+        </RouteStack.Navigator>
+    );
 }
 
 // 하단 탭 네비게이터 컴포넌트 (순서 : 히스토리 -> 경로생성 -> 마이페이지)
@@ -36,13 +66,12 @@ function BottomTabApp() {
 
             <Tab.Screen
                 name="Route"
-                component={RouteScreen}
+                component={RouteStackScreen} // 변경된 부분
                 options={{
-                    title: '경로생성',
+                    title: "경로생성",
                     tabBarIcon: ({ color, size }) => (
                         <Icon name="map" color={color} size={size} />
                     ),
-                    headerShown: false, // 상단 제목 숨기기
                 }}
             />
 
