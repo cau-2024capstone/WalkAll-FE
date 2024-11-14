4b2c73e8b0bd4d76d840797a7ce8d240709cafb3
ming9oo <mj10050203@gmail.com>
2024-11-14 16:33:10 +0900

    [fix] add filter and reload

--- src/main/screens/HistoryScreen.js
+++ src/main/screens/HistoryScreen.js
@@ -6,9 +6,11 @@ import HistoryCardArea from '../components/historyScreen/HistoryCardArea';
 import dummyData from '../components/historyScreen/dummyData';
 
 function HistoryScreen() {
-    const [searchResults, setSearchResults] = useState(
-        [...dummyData].sort((a, b) => new Date(b.date) - new Date(a.date))
-    );
+    const initialData = [...dummyData].sort((a, b) => new Date(b.date) - new Date(a.date));
+    const [searchResults, setSearchResults] = useState(initialData);
+
+    // 검색어 상태를 초기화하기 위해 참조를 생성
+    const searchInputRef = React.useRef();
 
     const handleSearch = (query) => {
         const filteredData = dummyData
@@ -19,9 +21,17 @@ function HistoryScreen() {
         setSearchResults(filteredData);
     };
 
+    const handleRefresh = () => {
+        setSearchResults(initialData);
+        // 검색어를 초기화하기 위해 HistorySearch 컴포넌트의 메서드 호출
+        if (searchInputRef.current) {
+            searchInputRef.current.clearSearch();
+        }
+    };
+
     return (
         <View style={localStyles.container}>
-            <HistorySearch onSearch={handleSearch} />
+            <HistorySearch onSearch={handleSearch} onRefresh={handleRefresh} ref={searchInputRef} />
             <HistoryCardArea data={searchResults} />
         </View>
     );
@@ -33,7 +43,7 @@ const localStyles = StyleSheet.create({
     container: {
         height: 760,
         paddingTop: 30,
-        width: 382,
+        width: 383,
         position: "relative",
         flexShrink: 0,
         flexDirection: "column",
